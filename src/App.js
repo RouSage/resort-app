2a2cbdbda63369feb5ec570f1c98b233194c6c1e
RouSage <therousage@gmail.com>
2020-08-20 12:35:03 +0300

    feat: basic pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Home />
+      <SingleRoom />
+      <Rooms />
+      <Error />
+    </>
+  );
 };
 
 export default App;
