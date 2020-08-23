1da1eb549b94b35c84a437ac6555305f8bb5e4ce
RouSage <therousage@gmail.com>
2020-08-23 15:30:06 +0300

    feat: featured rooms component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Banner from '../components/Banner';
+import FeaturedRooms from '../components/FeaturedRooms';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import { ROOMS_PAGE } from '../routes';
@@ -19,6 +20,7 @@ const Home = () => {
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 };
