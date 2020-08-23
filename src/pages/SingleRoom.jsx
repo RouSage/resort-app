d29a471be0d85d7f74be4bde0b240b3d647aa9c7
RouSage <therousage@gmail.com>
2020-08-23 16:47:53 +0300

    feat: display error on single room page on room abscence

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Banner from '../components/Banner';
+import Hero from '../components/Hero';
+import { RoomContext } from '../context';
+import { ROOMS_PAGE } from '../routes';
 
 const SingleRoom = () => {
-  return <div>Hello from Single Room page</div>;
+  const { slug } = useParams();
+  const { getRoom } = useContext(RoomContext);
+
+  const room = getRoom(slug);
+
+  if (!room) {
+    return (
+      <div className="error">
+        <h3>No such room could be found...</h3>
+        <Link to={ROOMS_PAGE} className="btn-primary">
+          Back to Rooms
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <Hero hero="roomsHero">
+      <Banner title={`${room.name} room`}>
+        <Link to={ROOMS_PAGE} className="btn-primary">
+          Back to Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default SingleRoom;
