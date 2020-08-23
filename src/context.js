1053ac1a93b8b85d06114e0fa1c66414e3639ebb
RouSage <therousage@gmail.com>
2020-08-23 15:04:57 +0300

    feat: format rooms data and extract featuredrooms

--- src/context.js
+++ src/context.js
@@ -1,10 +1,47 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import items from './data';
 
 export const RoomContext = React.createContext();
 
 const RoomProvider = ({ children }) => {
-  return <RoomContext.Provider value="bye">{children}</RoomContext.Provider>;
+  const [loading, setLoading] = useState(true);
+  const [roomsData, setRoomsData] = useState({
+    rooms: [],
+    featuredRooms: [],
+    sortedRooms: [],
+  });
+
+  const formatData = (data) => {
+    const tempItems = data.map((item) => {
+      const { id } = item.sys;
+      const images = item.fields.images.map((image) => image.fields.file.url);
+      const room = { ...item.fields, images, id };
+
+      return room;
+    });
+
+    return tempItems;
+  };
+
+  useEffect(() => {
+    const rooms = formatData(items);
+    const featuredRooms = rooms.filter((room) => room.featured);
+
+    setRoomsData((prevRoomsData) => ({
+      ...prevRoomsData,
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+    }));
+    setLoading(false);
+  }, []);
+
+  return (
+    <RoomContext.Provider value={{ ...roomsData, loading }}>
+      {children}
+    </RoomContext.Provider>
+  );
 };
 
 RoomProvider.propTypes = {
