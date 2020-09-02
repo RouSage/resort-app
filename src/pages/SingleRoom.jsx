082ca86be5479b296e00baf44ce3187ccc58a2ac
RouSage <therousage@gmail.com>
2020-09-02 16:29:32 +0300

    feat: single room page

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -23,14 +23,54 @@ const SingleRoom = () => {
     );
   }
 
+  const [mainImg, ...roomImages] = room.images;
+
   return (
-    <StyledHero img={room.images[0] || defaultImg}>
-      <Banner title={`${room.name} room`}>
-        <Link to={ROOMS_PAGE} className="btn-primary">
-          Back to Rooms
-        </Link>
-      </Banner>
-    </StyledHero>
+    <>
+      <StyledHero img={mainImg || defaultImg}>
+        <Banner title={`${room.name} room`}>
+          <Link to={ROOMS_PAGE} className="btn-primary">
+            Back to Rooms
+          </Link>
+        </Banner>
+      </StyledHero>
+      <section className="single-room">
+        <div className="single-room-images">
+          {roomImages.map((image, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <img key={index} src={image} alt={room.name} />
+          ))}
+        </div>
+        <div className="single-room-info">
+          <article className="desc">
+            <h3>Details</h3>
+            <p>{room.description}</p>
+          </article>
+          <article className="info">
+            <h3>Info</h3>
+            <h6>Price: ${room.price}</h6>
+            <h6>Size: {room.size} SQFT</h6>
+            <h6>
+              Max Capacity:
+              {room.capacity > 1
+                ? ` ${room.capacity} People`
+                : ` ${room.capacity} Person`}
+            </h6>
+            <h6>{room.pets ? 'Pets allowed' : 'No pets allowed'}</h6>
+            <h6>{room.breakfast && 'Free breakfast included'}</h6>
+          </article>
+        </div>
+      </section>
+      <section className="room-extras">
+        <h6>Extras</h6>
+        <ul className="extras">
+          {room.extras.map((extra, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={index}>- {extra}</li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 };
 
