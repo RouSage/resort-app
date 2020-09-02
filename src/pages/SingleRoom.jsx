b1932fcded3bac8c1cae7baad90894e8c4b122ea
RouSage <therousage@gmail.com>
2020-09-02 15:57:11 +0300

    feat: dynamic hero on single room page

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Banner from '../components/Banner';
-import Hero from '../components/Hero';
+import StyledHero from '../components/StyledHero';
 import { RoomContext } from '../context';
+import defaultImg from '../images/room-1.jpeg';
 import { ROOMS_PAGE } from '../routes';
 
 const SingleRoom = () => {
@@ -23,13 +24,13 @@ const SingleRoom = () => {
   }
 
   return (
-    <Hero hero="roomsHero">
+    <StyledHero img={room.images[0] || defaultImg}>
       <Banner title={`${room.name} room`}>
         <Link to={ROOMS_PAGE} className="btn-primary">
           Back to Rooms
         </Link>
       </Banner>
-    </Hero>
+    </StyledHero>
   );
 };
 
